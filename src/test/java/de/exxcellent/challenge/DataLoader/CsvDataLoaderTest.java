a993f0275c452d74074361d335969a8e364d1a39
Arthur <arthur.ulmer@gmx.net>
2024-09-10 21:26:35 +0200

    adjusted tests to the changes made corresponding to ChallengeDataStructure

--- src/test/java/de/exxcellent/challenge/DataLoader/CsvDataLoaderTest.java
+++ src/test/java/de/exxcellent/challenge/DataLoader/CsvDataLoaderTest.java
@@ -1,9 +1,12 @@
 package de.exxcellent.challenge.DataLoader;
 
-import de.exxcellent.challenge.Excepetions.WrongFileFormatException;
-import de.exxcellent.challenge.Excepetions.WrongFormatForTableException;
+import de.exxcellent.challenge.DataObjects.ChallengeDataStructure;
+import de.exxcellent.challenge.Exceptions.WrongFileFormatException;
+import de.exxcellent.challenge.Exceptions.WrongFormatForTableException;
 import org.junit.jupiter.api.Test;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -14,21 +17,77 @@ class CsvDataLoaderTest {
     void loadDataFromPath() {
         CsvDataLoader csvDataLoader = new CsvDataLoader();
 
-        List<List<String>> data = csvDataLoader.loadDataFromPathAsTable("src/test/resources/football.csv");
-        assertEquals(data.get(0).get(5), "Goals");
-        assertEquals(data.get(7).get(0), "West_Ham");
-        assertEquals(data.get(10).get(5), "55");
+        try {
+            List<List<String>> weatherData = csvDataLoader.loadDataFromPath("src/test/resources/weather.csv");
+            assertEquals(weatherData.get(0).get(1), "MxT");
+            assertEquals(weatherData.get(3).get(6), "350");
 
-        Exception dataCouldNotBeFoundException = assertThrows(RuntimeException.class,
-                () -> csvDataLoader.loadDataFromPathAsTable("fictional_path.csv"));
-        assertEquals(dataCouldNotBeFoundException.getMessage(), "Data to be loaded could not be located.");
+            List<List<String>> footballData = csvDataLoader.loadDataFromPath("src/test/resources/football.csv");
+            assertEquals(footballData.get(6).get(0), "Chelsea");
+            assertEquals(footballData.get(3).get(6), "45");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    @Test
+    void loadDataFromPathExceptionChecks() {
+        CsvDataLoader csvDataLoader = new CsvDataLoader();
+
+        Exception dataCouldNotBeFoundException = assertThrows(FileNotFoundException.class,
+                () -> csvDataLoader.loadDataFromPath("fictional_path.csv"));
+        assertEquals(dataCouldNotBeFoundException.getMessage(), "fictional_path.csv (Das System kann die angegebene Datei nicht finden)");
 
         Exception wrongFileFormatException = assertThrows(WrongFileFormatException.class,
-                () -> csvDataLoader.loadDataFromPathAsTable("src/test/resources/test.txt"));
+                () -> csvDataLoader.loadDataFromPath("src/test/resources/test.txt"));
         assertEquals(wrongFileFormatException.getMessage(), "The file path given does not lead to a CSV file.");
+    }
+
+    @Test
+    void loadDataFromPathDelimiterSetter() {
+        CsvDataLoader csvDataLoader = new CsvDataLoader();
+
+        try {
+            assertEquals(csvDataLoader.loadDataFromPath("src/test/resources/file_with_non_standard_delimiter.csv").get(0).get(0),
+                    "column1; column2; column3");
+
+            csvDataLoader.setDelimiter(";");
+
+            assertEquals(csvDataLoader.loadDataFromPath("src/test/resources/file_with_non_standard_delimiter.csv").get(0).get(0),
+                    "column1");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Test
+    void loadDataFromPathInChallengeStructure(){
+        CsvDataLoader csvDataLoader = new CsvDataLoader();
+
+        try {
+            ChallengeDataStructure weatherChallengeData = csvDataLoader.loadDataFromPathInChallengeStructure("src/test/resources/weather.csv");
+            assertEquals(weatherChallengeData.getIndexHeader(), "Day");
+            assertEquals(weatherChallengeData.getValueHeader().get(7), "Dir");
+            assertEquals(weatherChallengeData.getIndices().get(3), "4");
+            assertEquals(weatherChallengeData.getValues().get(6).get(3), 53.0);
+
+            ChallengeDataStructure footballChallengeData = csvDataLoader.loadDataFromPathInChallengeStructure("src/test/resources/football.csv");
+            assertEquals(footballChallengeData.getIndexHeader(), "Team");
+            assertEquals(footballChallengeData.getValueHeader().get(4), "Goals");
+            assertEquals(footballChallengeData.getIndices().get(6), "West_Ham");
+            assertEquals(footballChallengeData.getValues().get(9).get(5), 51.0);
+        } catch (IOException | WrongFormatForTableException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Test
+    void loadDataFromPathInChallengeStructureExceptionChecks(){
+        CsvDataLoader csvDataLoader = new CsvDataLoader();
 
         Exception wrongFormatForTableException = assertThrows(WrongFormatForTableException.class,
-                () -> csvDataLoader.loadDataFromPathAsTable("src/test/resources/weather_modified.csv"));
+                () -> csvDataLoader.loadDataFromPathInChallengeStructure("src/test/resources/weather_missing_a_value.csv"));
         assertEquals(wrongFormatForTableException.getMessage(),
                 "The number of columns in the file that was read are not consistent across all its lines.");
     }
